ca58718ab7b443c40deebc9ffd175efcd3635bf6
IT그룹_간수현 <shkan@englishegg.co.kr>
2023-04-25 14:44:13 +0900

    1

--- src/Test.js
+++ src/Test.js
@@ -6,11 +6,10 @@ function Test(props) {
             <video width={'50%'}
                    height={'50%'}
                    autoPlay
-                   playsInline
                    muted
                    controls
                    loop
-                   src="video/hht1_ep04_le01_mini_video.mp4" />
+                   src="video/hht1_ep01_le01_mini_video.mp4" />
 
 
         </div>
