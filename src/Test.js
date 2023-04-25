517382cf4810b9582e755d8c5f58fb6b3e2de006
IT그룹_간수현 <shkan@englishegg.co.kr>
2023-04-25 14:13:56 +0900

    test

---
+++ src/Test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import sample from './hht1_ep04_le01_mini_video.mp4'
+function Test(props) {
+    return (
+        <div>
+            <video width={'50%'}
+                   height={'50%'}
+                   autoPlay
+                   muted
+                   controls
+                   loop
+                   src="video/hht1_ep04_le01_mini_video.mp4" />
+
+
+        </div>
+
+    );
+}
+
+export default Test;
